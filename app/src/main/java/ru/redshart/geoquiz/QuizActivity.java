346cae9a67aa9ca48e814890463eaa1fd8118b5e
alexnice7272@gmail.com <Githubb4by>
2019-03-19 16:24:14 +0500

    Toast location get back to the default place Add new class Question Make array of different questions Add 2 new Buttons (Next, Prev) Add some arrows in res TextView shows question. Also you can change them with help of buttons

--- app/src/main/java/ru/redshart/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/redshart/geoquiz/QuizActivity.java
@@ -2,30 +2,42 @@ package ru.redshart.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import static android.widget.Toast.makeText;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private Button mPreviousButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_africa, true),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_mideast, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
+
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast t = makeText(QuizActivity.this, "Correct!", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP,0,200);
-                t.show();
+                checkAnswer(true);
             }
 
         });
@@ -35,11 +47,52 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast t = makeText(QuizActivity.this, "Incorrect!", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP,0,200);
-                t.show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton = findViewById(R.id.next_button); // ссылка на кнопку
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPreviousButton = findViewById(R.id.prev_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
     }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(QuizActivity.this,messageResId,Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+
 }
+
 
